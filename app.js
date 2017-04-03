58242fd1fbb46b3e4f55666a7df93afc70694c79
Jordan Stephenson <jordanwhitestephenson@gmail.com>
2017-04-03 15:51:13 -0600

    playing with freaking heroku

--- app.js
+++ app.js
@@ -7,23 +7,11 @@ const authorFile = require('./routes/authorroute')
 const bookFile = require('./routes/bookroute')
 
 app.options('*', cors());
-
-// app.use(cors({origin: '*'}))
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use('/author', authorFile)
 app.use('/book', bookFile)
-// app.use('/author_bookroute', authorbookFile)
-
-
 app.use(express.static('public'));
-
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
 
 app.listen(PORT, ()=> {
   console.log(`Listening on port ${PORT}`)
--- public/js/viewbooks.js
+++ public/js/viewbooks.js
@@ -5,20 +5,12 @@ $(document).ready(function() {
           url: `https://jreads.herokuapp.com/book`,
           contentType: "application/json",
           data: JSON.stringify()
-      }).then(function(success) {
-          console.log(success)
+      }).then(function(data) {
+          console.log(data)
+          for (var j = 0; j < data.length; j++) {
+                  $('.bookTitle').append('<tr><td>' + '<p>' + data[j].title + '</p>' + '</tr></td>');
+              }
 
       });
 
-
-
-    // $.get('https://jreads.herokuapp.com/book', function(data) {
-    //   console.log(data)
-    //     // for (var j = 0; j < data.length; j++) {
-    //     //
-    //     //         $('.bookTitle').append('<tr><td>' + data[j].title + '</tr></td>');
-    //
-    //
-    //         }
-    //     );
 });
