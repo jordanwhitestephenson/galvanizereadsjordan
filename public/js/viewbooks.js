038634ad6121483cfdf9cd114c7212a0c392f596
Jordan Stephenson <jordanwhitestephenson@gmail.com>
2017-04-03 16:32:53 -0600

    s

--- public/js/viewbooks.js
+++ public/js/viewbooks.js
@@ -9,13 +9,10 @@ $(document).ready(function() {
           console.log(data)
           // <p class="post-meta" id = "shortsentence"> ${data[i].body} </p>
           for (var j = 0; j < data.length; j++) {
-                  $('.bookRow').append(`<tr><td class ="bookdata"> ${data[j].title} </td> <td class= "bookdata"> ${data[j].genre} </td><td class ="bookdata" id="shortsentence"> ${data[j].descripton} </td> <td class = "bookdata">${data[j].cover_img_url}</td></tr>`);
 
+                  $('.bookBody').append(`<tr class = "bookRow"><td class = "bookdata"> ${data[j].title} </td><td class = "bookdata"> ${data[j].genre} </td> <td class = "bookdata"> ${data[j].description} </td> <td class = "bookdata"> ${data[j].cover_img_url} </td><tr>`)
 
-
-                  // $('.bookGenre').append('<tr>' + '<td class = "bookdata">' + data[j].genre + '</tr>' + '</td>')
-                  // $('.bookDescription').append('<tr id = "shortsentence">' + '<td class = "bookdata" id= "shortsentence">' + data[j].description + '</tr>' + '</td>')
-                  // $('.bookImg').append('<tr>' + '<td class = "bookdata">' + data[j].cover_img_url+ '</tr>' + '</td>')
+  
               }
 
       });
