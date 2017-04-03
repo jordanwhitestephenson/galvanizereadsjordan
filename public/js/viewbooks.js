4fef7b765b49222440f09582fa8a25a6de81fea4
Jordan Stephenson <jordanwhitestephenson@gmail.com>
2017-04-03 15:58:29 -0600

    appedning

--- public/js/viewbooks.js
+++ public/js/viewbooks.js
@@ -8,7 +8,8 @@ $(document).ready(function() {
       }).then(function(data) {
           console.log(data)
           for (var j = 0; j < data.length; j++) {
-                  $('.bookTitle').append('<tr><td>' + data[j].title + '</tr></td>');
+                  $('.bookTitle').append('<tr>' + '<td>' + data[j].title + '</tr>' + '</td>');
+                  $('.bookGenre').append('<tr>' + '<td>' + data[j].genre + '</tr>' + '</td>')
               }
 
       });
